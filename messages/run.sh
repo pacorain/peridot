c37effc4308e0b4b83579f8bc5e5ee44fff7cd95
Paco DeArmond <paco@heckin.io>
2019-01-27 17:34:12 -0600

    Fix errors

--- messages/run.sh
+++ messages/run.sh
@@ -1,2 +1,2 @@
 open /Applications/Messages.app
-/usr/local/bin/wbconsole -script=/Users/paco/Documents/SQL/message_get.sql
+/usr/local/bin/wbconsole -script=/scripts/messages/messages.workbench.sql
